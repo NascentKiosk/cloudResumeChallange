42f8586a405d19b988d8ad3b61777a587258ffe0
Mmbasu <mmbasujuma@gmail.com>
2026-06-11 15:05:37 +0200

    updated functional file

--- backend/api/backend/function_app.py
+++ backend/api/backend/function_app.py
@@ -13,14 +13,23 @@ db = client.get_database_client("resume-db")
 container = db.get_container_client("counter")
 
 @app.function_name(name="visitorCounter")
-@app.route(route="visitorCounter")
+@app.route(
+    route="visitorCounter",
+    auth_level=func.AuthLevel.ANONYMOUS
+)
 def visitorCounter(req: func.HttpRequest) -> func.HttpResponse:
 
-    item = container.read_item("visitorCount", partition_key="visitorCount")
+    item = container.read_item(
+        item="visitorCount",
+        partition_key="visitorCount"
+    )
 
     item["count"] += 1
 
-    container.replace_item(item["id"], item)
+    container.replace_item(
+        item=item["id"],
+        body=item
+    )
 
     return func.HttpResponse(
         json.dumps({"count": item["count"]}),
